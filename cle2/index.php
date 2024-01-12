de6c0a4fe02aa568817faa301f0b067d1c1d91c6
Sil <silvangemeren@gmail.com>
2024-01-12 15:21:04 +0100

    Homepage

--- cle2/index.php
+++ cle2/index.php
@@ -1,13 +1,34 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CLE 2</title>
 </head>
+
+<header>
+
+    <nav>
+        <div class="logo"><img src="images/logo_144.png" alt=""></div>
+
+        <div class="home"><a href="">Home</a></div>
+        <div class="dag"><a href="">Hoe ziet een dag eruit </a></div>
+        <div class="fotos"><a href="">Foto's</a></div>
+        <div class="afspraak"><a href="">Afspraak maken</a> </div>
+        <div class="contact"><a href="">Contact en locatie</a></div>
+        <div class="inlog"><a href="">Inlog</a></div>
+
+
+
+    </nav>
+</header>
+
 <body>
-    Home page
+
+
+
 </body>
+
 </html>
