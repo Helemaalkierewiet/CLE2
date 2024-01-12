a4de10581c97fadb23fe19c1b9a105ebfd531d15
RoelVelis <Roelenjan@gmail.com>
2024-01-12 12:53:45 +0100

    Details updated

--- cle2/details.php
+++ cle2/details.php
@@ -9,6 +9,7 @@ require_once 'includes/database.php';
 $query ="SELECT *
 FROM appointments
 JOIN users ON appointments.user_id = users.id
+JOIN timeslots ON appointments.timeslot_id = timeslots.id
 JOIN days ON appointments.day_id = days.id";
 
 $stmt = mysqli_prepare($db, $query);
@@ -46,9 +47,13 @@ $index = (isset($_GET['id']) && is_numeric($_GET['id'])) ? htmlspecialchars($_GE
             <ul>
                 <li>Voornaam: <?= isset($appointments[$index]['first_name']) ? htmlspecialchars($appointments[$index]['first_name']) : '' ?></li>
                 <li>Achternaam: <?= isset($appointments[$index]['last_name']) ? htmlspecialchars($appointments[$index]['last_name']) : '' ?></li>
+                <li>Datum: <?= isset($appointments[$index]['date']) ? htmlspecialchars($appointments[$index]['date']) : '' ?></li>
                 <li>Dag: <?= isset($appointments[$index]['name']) ? htmlspecialchars($appointments[$index]['name']) : '' ?></li>
                 <li>Maand: <?= isset($appointments[$index]['month']) ? htmlspecialchars($appointments[$index]['month']) : '' ?></li>
                 <li>Jaar: <?= isset($appointments[$index]['year']) ? htmlspecialchars($appointments[$index]['year']) : '' ?></li>
+                <li>Tijdslot: <?= isset($appointments[$index]['timeslot_id']) ? htmlspecialchars($appointments[$index]['timeslot_id']) : '' ?></li>
+                <li>Start tijd: <?= isset($appointments[$index]['start_time']) ? htmlspecialchars($appointments[$index]['start_time']) : '' ?></li>
+                <li>Eind tijd: <?= isset($appointments[$index]['end_time']) ? htmlspecialchars($appointments[$index]['end_time']) : '' ?></li>
                 <li>Beschrijving: <?= isset($appointments[$index]['description']) ? htmlspecialchars($appointments[$index]['description']) : '' ?></li>
             </ul>
         </section>
