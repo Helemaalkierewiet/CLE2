8727563d3f3948aca12fe2cf354fe5ed93e2743a
Sil <silvangemeren@gmail.com>
2024-01-16 13:15:08 +0100

    Homepage

--- cle2/index.php
+++ cle2/index.php
@@ -5,30 +5,316 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,500;1,800&display=swap"
     <title>CLE 2</title>
 </head>
 
-<header>
-
-    <nav>
-        <div class="logo"><img src="images/logo_144.png" alt=""></div>
-
-        <div class="home"><a href="">Home</a></div>
-        <div class="dag"><a href="">Hoe ziet een dag eruit </a></div>
-        <div class="fotos"><a href="">Foto's</a></div>
-        <div class="afspraak"><a href="">Afspraak maken</a> </div>
-        <div class="contact"><a href="">Contact en locatie</a></div>
-        <div class="inlog"><a href="">Inlog</a></div>
-
-
-
-    </nav>
-</header>
-
 <body>
+<div class="welcome-frame">
+    <section class="home-frame">
+        <h3 class="welkom-op-boerderij">Welkom op Boerderij Weidelicht!</h3>
+        <div class="appointment-frame">
+            <header class="screenshot-2023-12-11-135019-1-parent">
+                <img
+                        class="screenshot-2023-12-11-135019-1"
+                        loading="eager"
+                        alt=""
+                        src="images/logo_144.png"
+                />
 
+                <div class="frame-parent">
+                    <div class="vector-parent">
+                        <img
+                                class="frame-child"
+                                alt=""
+                                src="./public/rectangle-14.svg"
+                        />
 
+                        <a
+                                class="home"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Home</a
+                        >
+                    </div>
+                    <div class="vector-group">
+                        <img
+                                class="frame-item"
+                                alt=""
+                                src="./public/rectangle-15.svg"
+                        />
 
+                        <a
+                                class="hoe-ziet-een"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Hoe ziet een dag eruit</a
+                        >
+                    </div>
+                    <div class="vector-container">
+                        <img
+                                class="frame-inner"
+                                alt=""
+                                src="./public/rectangle-16.svg"
+                        />
+
+                        <a
+                                class="fotos"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Foto´s</a
+                        >
+                    </div>
+                    <div class="frame-div">
+                        <img
+                                class="rectangle-icon"
+                                alt=""
+                                src="./public/rectangle-18.svg"
+                        />
+
+                        <a
+                                class="afspraak-maken"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Afspraak maken</a
+                        >
+                    </div>
+                    <div class="vector-parent1">
+                        <img
+                                class="frame-child1"
+                                alt=""
+                                src="./public/rectangle-17.svg"
+                        />
+
+                        <a
+                                class="contact-locatie"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Contact & Locatie</a
+                        >
+                    </div>
+                    <button class="frame-button">
+                        <img
+                                class="frame-child2"
+                                alt=""
+                                src="./public/rectangle-19.svg"
+                        />
+
+                        <a
+                                class="login"
+                                href="https://www.figma.com/proto/OfRFP7CCgVH9oy64zSfQ4F/Untitled?type=design&node-id=21-43&t=5shvpIsrMsa1bECF-0&scaling=min-zoom&page-id=0%3A1"
+                                target="_blank"
+                        >Login</a
+                        >
+                    </button>
+                </div>
+            </header>
+            <div class="wrapper-image-1">
+                <img
+                        class="image-1-icon"
+                        loading="eager"
+                        alt=""
+                        src="images/Boerderij-Weidelicht-voortuin-2048x1161.jpg"
+                />
+            </div>
+        </div>
+    </section>
+    <section class="info-frame">
+        <div class="text">
+            <div class="text1">
+                <div class="midden-in-het-container">
+              <span>
+                <p class="blank-line">
+                  <span>
+                    <span>&nbsp;</span>
+                  </span>
+                </p>
+                <p class="blank-line1">
+                  <span>
+                    <span>&nbsp;</span>
+                  </span>
+                </p>
+                <p class="midden-in-het-groene-hart-van">
+                  <span>
+                    <span class="midden-in-het"
+                    >Midden in het groene hart van Midden-Delfland bieden wij
+                      kleinschalige dagbesteding voor ouderen met dementie. Een
+                      warme, veilige en liefdevolle plek waar wij de gasten en
+                      hun mantelzorgers met open armen ontvangen. Wij dragen
+                      zorg om te ontzorgen, door de gasten en mantelzorgers in
+                      een huiselijke en ongedwongen sfeer mee te laten genieten
+                      van onze heerlijke en unieke locatie.</span
+                    >
+                  </span>
+                </p>
+                <p class="blank-line2">&nbsp;</p>
+              </span>
+                </div>
+            </div>
+            <div class="text2">
+                <div class="wat-zeggen-anderen">Wat zeggen anderen over ons?</div>
+            </div>
+            <div class="text3">
+                <div class="text4"></div>
+                <h3 class="wat-een-geweldige">Wat een Geweldige plek is dit!</h3>
+                <div class="text5">
+                    <div class="met-prachtige-mooie-container">
+                <span>
+                  <p class="blank-line3">&nbsp;</p>
+                  <p class="met-prachtige-mooie">
+                    “Met prachtige mooie ruimten, waarin een ieder echt zijn
+                    eigen plekje kan vinden, met uitzicht op de weiden, lekker
+                    bij de grote open haard, met anderen rond de grote tafel,
+                    zingend met de pianist of de oude platen, een rustig plekje
+                    in de bibliotheek of een ommetje naar de dieren.
+                  </p>
+                  <p class="een-plek-met">
+                    Een plek met heerlijke koffie en versgebakken lekkernijen,
+                    steeds wisselende activiteiten en vooral… veel respect en
+                    mooie aandacht. Zelfs voor de begeleiders, die welkom zijn,
+                    ook voor koffie en warme lunch! En dan heb ik het nog niet
+                    over het ritje erheen door het prachtige Midden-Delfland…!
+                    Mijn oprechte complimenten voor de initiatiefnemer(s?) en de
+                    aandachtige medewerkers!”
+                  </p>
+                  <p class="blank-line4">
+                    <a
+                            class="blank-line5"
+                            href="https://boerderijweidelicht.nl/next-slide"
+                            target="_blank"
+                    >
+                      <span class="blank-line6">&nbsp;</span>
+                    </a>
+                  </p>
+                  <p class="blank-line7">
+                    <a
+                            class="blank-line8"
+                            href="https://boerderijweidelicht.nl/next-slide"
+                            target="_blank"
+                    >
+                      <span class="blank-line9">&nbsp;</span>
+                    </a>
+                  </p>
+                  <p class="blank-line10">
+                    <a
+                            class="blank-line11"
+                            href="https://boerderijweidelicht.nl/next-slide"
+                            target="_blank"
+                    >
+                      <span class="blank-line12">&nbsp;</span>
+                    </a>
+                  </p>
+                  <p class="blank-line13">&nbsp;</p>
+                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="rectangle-wrapper">
+        <div class="rectangle">
+            <div class="rectangle-child"></div>
+            <div class="rectangle-inner">
+                <div class="frame-group">
+                    <div class="text-parent">
+                        <div class="text6">
+                            <div class="text-group">
+                                <div class="text7">
+                                    <i class="contact">Contact</i>
+                                    <div class="line">
+                                        <div class="whatsapp-logo-frame">
+                                            <img
+                                                    class="whatsapp-logo-1"
+                                                    loading="eager"
+                                                    alt=""
+                                                    src="images/whatsapp%20logo.png"
+                                            />
+
+                                            <div class="whatsapp-06-">
+                                                whatsapp: 06 - 27 51 09 11
+                                            </div>
+                                        </div>
+                                        <div class="whatsapp-logo-frame1">
+                                            <img
+                                                    class="telefoon-logo-1"
+                                                    loading="eager"
+                                                    alt=""
+                                                    src="images/telefoon%20logo.jpg"
+                                            />
+
+                                            <div class="div">015-3809999</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="locatie-logo-frame">
+                                    <img
+                                            class="locatie-logo-1"
+                                            loading="eager"
+                                            alt=""
+                                            src="images/locatie%20logo.jpg"
+                                    />
+
+                                    <div class="oostveenseweg-7-2636">
+                                        Oostveenseweg 7, 2636 EC Schipluiden
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mail-logo-frame">
+                                <img
+                                        class="mail-logo-1"
+                                        loading="eager"
+                                        alt=""
+                                        src="images/mail%20logo.jpg"
+                                />
+
+                                <div class="infoboerderijweidelichtnl">
+                                    info@boerderijweidelicht.nl
+                                </div>
+                            </div>
+                        </div>
+                        <div class="locatie-parent">
+                            <i class="locatie">Locatie</i>
+                            <div class="locatieboederij-1-wrapper">
+                                <img
+                                        class="locatieboederij-1-icon"
+                                        loading="eager"
+                                        alt=""
+                                        src="images/locatieboederij.png"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nieuws-frame">
+                        <i class="nieuws">Nieuws</i>
+                        <div class="de-keuken-het-container">
+                  <span>
+                    <p class="de-keuken-het">
+                      De keuken: Het middelpunt van een huishouden, ook bij ons!
+                    </p>
+                    <p class="blank-line14">&nbsp;</p>
+                    <p class="en-toen-was">
+                      En toen was er ineens.... een kalfje!
+                    </p>
+                    <p class="blank-line15">&nbsp;</p>
+                    <p class="heel-blij-met">Heel blij met de duo-fiets!</p>
+                  </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="line-element">
+                <div class="copyright-frame"></div>
+                <div class="copyright-2019-all-rights-re-wrapper">
+                    <div class="copyright-2019">
+                        Copyright 2019 | All Rights Reserved by Boerderij Weidelicht
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </section>
+</div>
 </body>
 
 </html>
--- cle2/testhome.php
+++ cle2/testhome.php
@@ -73,3 +73,238 @@ width: 100%;
 }
 
 }
+
+
+
+
+2de
+
+<header>
+
+    <body>
+    <nav>
+        <div class="logo"><img src="images/logo_144.png" alt=""></div>
+
+        <div class="Frame-3">
+            <div class="Rectangle-14">
+                <span class="Home">Home</span>
+            </div>
+
+            <div class="Rectangle-15">
+                <span class="Hoe-ziet-een-dag-eruit">
+                    Hoe ziet een dag eruit</span>
+            </div>
+
+            <div class="Rectangle-16">
+                <span class="Fotos">Foto´s</span>
+            </div>
+
+            <div class="Rectangle-18">
+                <span class="Afspraak-maken">
+                    Afspraak maken</span>
+            </div>
+
+            <div class="Rectangle-17">
+                <span class="Contact-Locatie">
+                    Contact & Locatie</span>
+            </div>
+
+            <div class="Rectangle-19">
+                <span class="Login">Login</span>
+            </div>
+
+        </div>
+
+    </nav>
+
+</header>
+
+
+<section>
+
+    <div class="boerderij"><img src="images/Boerderij-Weidelicht-voortuin-2048x1161.jpg"></div>
+
+</section>
+
+<footer>
+
+</footer>
+
+</body>
+
+css
+
+body{
+display: flex;
+width: 1440px;
+height: 1369px;
+flex-grow: 0;
+background-color: #fff;
+
+}
+
+nav{
+display: flex;
+flex-direction: row;
+}
+.logo img{
+width: 216px;
+height: 75px;
+margin: 0 0 0 188px;
+}
+
+.Frame-3 {
+display: flex;
+flex-direction: row;
+width: 848px;
+height: 75px;
+margin: 0 188px 0 0;
+padding: 22px 40px 22px 47px;
+background-color: rgba(255, 255, 255, 0.55);
+}
+
+
+.Rectangle-14 {
+width: 89px;
+height: 31px;
+margin: 0 24px 0 0;
+padding: 2px 11px 3px;
+border-radius: 30px;
+background-color: #438a4b;
+}
+
+.Home {
+width: 67px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.Rectangle-15 {
+width: 162px;
+height: 31px;
+margin: 0 24px;
+padding: 2px 12px 3px;
+border-radius: 30px;
+background-color: #76a02d;
+}
+
+.Hoe-ziet-een-dag-eruit {
+width: 138px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.Rectangle-16 {
+width: 89px;
+height: 31px;
+margin: 0 24px;
+padding: 2px 11px 3px;
+border-radius: 30px;
+background-color: #21a69d;
+}
+
+.Fotos {
+width: 67px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.Rectangle-18 {
+width: 107px;
+height: 31px;
+margin: 0 23px 0 24px;
+padding: 2px 10px 3px;
+border-radius: 30px;
+background-color: #8fad32;
+}
+
+.Afspraak-maken {
+width: 87px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.Rectangle-17 {
+width: 106px;
+height: 31px;
+margin: 0 24px 0 23px;
+padding: 2px 9px 3px 10px;
+border-radius: 30px;
+background-color: #448639;
+}
+
+.Contact-Locatie {
+width: 87px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.Rectangle-19 {
+width: 89px;
+height: 31px;
+margin: 0 0 0 24px;
+padding: 2px 11px 3px;
+border-radius: 30px;
+background-color: #318355;
+}
+
+.Login {
+width: 67px;
+height: 26px;
+font-family: Inter;
+font-size: 9px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #fff;
+}
+
+.boerderij img{
+width: 1064px;
+height: 410px;
+margin: 75px 188px 28px;
+box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+}
